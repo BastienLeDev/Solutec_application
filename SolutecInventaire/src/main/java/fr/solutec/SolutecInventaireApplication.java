b2866bf0b333655d660b88d94d86a03b5b03cab0
BastienLeDev <gauthiero.bastien@outlook.com>
2023-03-08 15:47:20 +0100

    First commit

---
+++ SolutecInventaire/src/main/java/fr/solutec/SolutecInventaireApplication.java
@@ -0,0 +1,21 @@
+package fr.solutec;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutecInventaireApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutecInventaireApplication.class, args);
+		System.out.println("Lancement terminé");
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Lancement en cours");
+		
+	}
+
+}
