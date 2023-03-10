eb48277ce2fa0e0d764ac6e76de781f5e09ba85d
BastienLeDev <gauthiero.bastien@outlook.com>
2023-03-10 17:55:38 +0100

    API

--- SolutecInventaire/src/main/java/fr/solutec/repository/ProductRepository.java
+++ SolutecInventaire/src/main/java/fr/solutec/repository/ProductRepository.java
@@ -1,10 +1,23 @@
 package fr.solutec.repository;
 
+import java.sql.Date;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
+	
 	public Iterable<Product> findByNameProduct(String nameProduct);
+	
+	public Iterable<Product> findByInventory(long inventory);
+	
+	@Query("SELECT p FROM Product p WHERE owner LIKE %?1%")
+	public Iterable<Product> findWithOwner(String name);
+	
+	public Iterable<Product> findByEntryDate(Date entryDate);
+	
+	public Iterable<Product> findByExitDate(Date exitDate);
 
 }
--- SolutecInventaire/src/main/java/fr/solutec/rest/ProductRest.java
+++ SolutecInventaire/src/main/java/fr/solutec/rest/ProductRest.java
@@ -1,7 +1,11 @@
 package fr.solutec.rest;
 
+import java.sql.Date;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +19,45 @@ public class ProductRest {
 	@Autowired
 	private ProductRepository productRepo;
 	
-	@GetMapping("filter/{nameProduct}")
+	@GetMapping("liste") // API pour avoir la liste de tout le matériel
+	public Iterable<Product> getAll(){
+		return productRepo.findAll();
+	}
+	
+	@GetMapping("filter1/{nameProduct}") //API pour filtrer par type de matériel
 	public Iterable<Product> getByProduct(@PathVariable String nameProduct){
 		return productRepo.findByNameProduct(nameProduct);
+	}
+	
+	@GetMapping("filter2/{inventory}")//API pour filtrer par n° d'inventaire
+	public Iterable<Product> getByInventory(@PathVariable long inventory){
+		return productRepo.findByInventory(inventory);
+	}
+	
+	@GetMapping("filter3/{owner}")// API pour filtrer par responsable
+	public Iterable<Product> getByOwner(@PathVariable String owner){
+		return productRepo.findWithOwner(owner);
+	}
+	
+	@GetMapping("filter4/{entryDate}") //API Filtrer par date d'entrée
+	public Iterable<Product> getByEntryDate(@PathVariable Date entryDate){
+		return productRepo.findByEntryDate(entryDate);
+	}
+	
+	@GetMapping("filter5/{exitDate}") //API Filtrer par date d'entrée
+	public Iterable<Product> getByExitDate(@PathVariable Date exitDate){
+		return productRepo.findByExitDate(exitDate);
+	}
+	
+	@DeleteMapping("delete/{idProduct}") //API Supprimer un article
+	public Boolean deleteProduct(@PathVariable Long idProduct){
+		Optional<Product> p = productRepo.findById(idProduct);
+		if(p.get() != null) {
+			productRepo.deleteById(idProduct);
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
 }
