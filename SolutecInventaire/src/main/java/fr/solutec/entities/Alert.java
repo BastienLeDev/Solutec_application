a79b2bf01e7fb33b1c8068ac21a7c764029dcb0d
BastienLeDev <gauthiero.bastien@outlook.com>
2023-04-12 11:30:47 +0200

    modifs API classe alerte

--- SolutecInventaire/src/main/java/fr/solutec/entities/Alert.java
+++ SolutecInventaire/src/main/java/fr/solutec/entities/Alert.java
@@ -1,16 +1,13 @@
 package fr.solutec.entities;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
-import jakarta.persistence.CascadeType;
+
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,8 +20,7 @@ public class Alert {
 	@Id @GeneratedValue
 	private Long idAlert;
 	private String nameAlert;
-	@OneToMany(cascade = CascadeType.ALL)
-	private Collection<Product> products = new ArrayList<>();
+	private List<String> products = new ArrayList<>();
 	private int level;
 	private boolean state;
 	private boolean triggered ;
--- SolutecInventaire/src/main/java/fr/solutec/rest/AlertRest.java
+++ SolutecInventaire/src/main/java/fr/solutec/rest/AlertRest.java
@@ -1,13 +1,19 @@
 package fr.solutec.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Alert;
+import fr.solutec.entities.Product;
 import fr.solutec.repository.AlertRepository;
 
 @CrossOrigin("*")
@@ -26,6 +32,26 @@ public class AlertRest {
 	public Iterable<Alert> alertHistory(){
 		return alertRepo.findAll();
 	}
+	
+	@PatchMapping("modifyAlert") //API pour modifier une alerte
+	public Alert modifyAlert(@RequestBody Alert alert) {
+		Alert a = alertRepo.findById(alert.getIdAlert()).get();
+		a.setNameAlert(alert.getNameAlert());
+		a.setLevel(alert.getLevel());
+		a.setNotifEmail(alert.isNotifEmail());
+		a.setProducts(alert.getProducts());
+		a.setState(alert.isState());
+		a.setTriggered(alert.isTriggered());
+		alertRepo.save(a);
+		return a;
+	}
+	
+	@DeleteMapping("deleteAlert/{idAlert}")
+	public boolean deleteAlert(@PathVariable Long idAlert){
+		alertRepo.deleteById(idAlert);
+		return true;
+	}
+	
 	
 	@GetMapping("getTrigger") //API pour voir les alertes déclenchées
 	public Iterable<Alert> triggerHistory(){
