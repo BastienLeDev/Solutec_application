d2e16aa7a59a7889a9c6b7f25f12803b31e34b6b
BastienLeDev <gauthiero.bastien@outlook.com>
2023-06-09 10:44:05 +0200

    Test unitaires d'API

--- SolutecInventaire/src/test/java/fr/solutec/rest/alertRestTest.java
+++ SolutecInventaire/src/test/java/fr/solutec/rest/alertRestTest.java
@@ -12,8 +12,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fr.solutec.entities.Alert;
+import fr.solutec.repository.AlertRepository;
 
 
 @RunWith(SpringRunner.class)
@@ -22,18 +29,45 @@ import org.springframework.test.web.servlet.MockMvc;
 public class alertRestTest {
 	@Autowired
 	private MockMvc mockMvc;
+	@Autowired
+	private AlertRepository alertRepo;
 	
-	// A revoir car accès refusé avec Spring security + pas d'alerte fictive
 	@Test
+	@WithMockUser(username = "UserTest", roles = "ADMIN")
 	public void modifyAlertTest() throws Exception {
-		mockMvc.perform(patch("/modifyAlert/1"))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$[1].alerte", is("test")))
-			.andExpect(jsonPath("$[2].seuil", is(1)))
-			.andExpect(jsonPath("$[3].active", is(false)))
-			.andExpect(jsonPath("$[4].triggered", is(false)))
-			.andExpect(jsonPath("$[5].Email", is(false)));
+		//Alerte fictive avant modification
+		Alert a = new Alert();
+		a.setAlerte("Alerte");
+		a.setIdAlert((long) 1);
+		a.setSeuil(0);
+		a.setActive(false);
+		a.setEmail(false);
+		a.setTriggered(false);
+		alertRepo.save(a);
 		
+		//Alerte fictive après modification
+		Alert a2 = new Alert();
+		a2.setAlerte("Alerte_après_Test");
+		a2.setSeuil(10);
+		a2.setActive(true);
+		a2.setEmail(false);
+		a2.setTriggered(false);
+		
+		mockMvc.perform(patch("/modifyAlert/{idAlert}",a.getIdAlert())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(a2))
+				)
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.alerte").value("Alerte_après_Test"))
+			.andExpect(jsonPath("$.seuil").value(10))
+			.andExpect(jsonPath("$.active").value(true))
+			.andExpect(jsonPath("$.triggered").value(false))
+			.andExpect(jsonPath("$.email").value(false));
 	}
+	
+	private String asJsonString(Object obj) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(obj);
+    }
 
 }
--- SolutecInventaire/src/test/java/fr/solutec/rest/productRestTest.java
+++ SolutecInventaire/src/test/java/fr/solutec/rest/productRestTest.java
@@ -5,24 +5,58 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fr.solutec.entities.Product;
+import fr.solutec.entities.TypeProduct;
+import fr.solutec.repository.ProductRepository;
+import fr.solutec.repository.TypeProductRepository;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class productRestTest {
+	
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private TypeProductRepository typeproductRepo;
+    
     @Test
+    @WithMockUser(username = "UserTest", roles = "ADMIN")
     public void addProductTest() throws Exception{
-        mockMvc.perform(patch("/add/database/UserTest").content("[{'typeProduct':{'nameProduct':'Type},'refProduct':'0000'}]"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[1].typeProduct.nameProduct", is("Type")))
-                .andExpect(jsonPath("$[2].refProduct", is("0000")));
+    	//Créer un type de produit fictif 
+        TypeProduct t1 = new TypeProduct(null,"type");
+        typeproductRepo.save(t1);
+        
+    	// Créer un objet de test pour le produit
+        Product product = new Product();
+        product.setTypeProduct(t1);
+        product.setRefProduct("ref");
+        product.setOwner("owner");
+        
+        //Appel de l'API à tester :
+         mockMvc.perform(post("/add/database/{login}","UserTest")	
+ 				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(product)))
+        		.andExpect(status().isOk());
+    }
+    
+    private String asJsonString(Object obj) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(obj);
     }
 }
